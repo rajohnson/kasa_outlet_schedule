098f152506e762969320e900c134a6c9b47f90f5
Rob Johnson <rjohnson@dcs-inc.net>
2023-08-11 16:29:32 -0700

    Different settings based on time of day

--- plug_schedule.py
+++ plug_schedule.py
@@ -1,15 +1,31 @@
 import asyncio
+import datetime
+
 import kasa
+
 from config import ip_address  # config just declares ip_address as a string
 
-ON_TIME_S = 5 * 60
-OFF_TIME_S = 25 * 60
+DAY_START_HOUR = 9
+DAY_END_HOUR = 19
+
+DAY_ON_TIME_S = 14
+DAY_OFF_TIME_S = 0.7 * 60
+
+NIGHT_ON_TIME_S = 10
+NIGHT_OFF_TIME_S = 1.4 * 60
+
 RETRY_DELAY_S = 5
 
 
 async def main() -> None:
     device = kasa.SmartStrip(ip_address)
     while True:
+        if DAY_START_HOUR <= datetime.datetime.now().hour < DAY_END_HOUR:
+            ON_TIME_S = DAY_ON_TIME_S
+            OFF_TIME_S = DAY_OFF_TIME_S
+        else:
+            ON_TIME_S = NIGHT_ON_TIME_S
+            OFF_TIME_S = NIGHT_OFF_TIME_S
         try:
             await device.turn_on()
             await asyncio.sleep(ON_TIME_S)
